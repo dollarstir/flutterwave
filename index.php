64e2c417ad5413722d30a0c244efc2902df66175
dollarstir <kpin463@gmail.com>
2022-03-08 07:34:43 +0000

    flutterwave

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- <script src="jquery.min.js"></script> -->
+    <script src="momo.js"></script>
+    <title>Mobile Money</title>
+</head>
+<body>
+
+ 
+ <form action="">
+    <input type="text" value="2" id="am">
+
+    <button type="button" onClick="payWithRave()" id="cc">Pay Now</button>
+ </form>
+
+
+
+    
+</body>
+</html>
---
+++ momo.js
@@ -0,0 +1,37 @@
+const API_publicKey = "FLWPUBK-2bf0cc595c642fe7b7f11b419497aed7-X";
+
+    function payWithRave() {
+        var x = getpaidSetup({
+            PBFPubKey: API_publicKey,
+            customer_email: "kpin463@gmail.com",
+            amount: 1,
+            customer_phone: "0556676471",
+            currency: "GHS",
+            country: "GH",
+            txref: "rave-123029278392",
+            meta: [{
+                metaname: "flightID",
+                metavalue: "AP1234"
+            }],
+            onclose: function() {},
+            callback: function(response) {
+                var txref = response.tx.txRef; // collect txRef returned and pass to a 					server page to complete status check.
+                console.log("This is the response returned after a charge", response);
+                if (
+                    response.tx.chargeResponseCode == "00" ||
+                    response.tx.chargeResponseCode == "0"
+                ) {
+                    // redirect to a success page
+                } else {
+                    // redirect to a failure page.
+                }
+
+                x.close(); // use this to close the modal immediately after payment.
+            }
+        });
+    }
+
+
+    // $(function(){
+
+    // })
